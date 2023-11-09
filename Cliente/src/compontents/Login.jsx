17fe6f66daae652c265f6d78228fb52483111876
Emadlg <osmanemanuel2004@gmail.com>
2023-11-08 22:54:44 -0600

    creacion de vista login

---
+++ Cliente/src/compontents/Login.jsx
@@ -0,0 +1,57 @@
+import './Login.css'
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBInput
+}
+from 'mdb-react-ui-kit';
+
+function App() {
+  return (
+    <MDBContainer className="my-5">
+
+      <MDBCard>
+        <MDBRow className='g-0'>
+
+          <MDBCol md='6'>
+            <MDBCardImage src='https://previews.123rf.com/images/elenabsl/elenabsl2005/elenabsl200500008/146948517-caja-de-donaci%C3%B3n-con-ropa-y-zapatos-variados-sobre-fondo-blanco-concepto-de-solidaridad-y-caridad.jpg' alt="login form" className='rounded-start w-100'/>
+          </MDBCol>
+
+          <MDBCol md='6'>
+            <MDBCardBody className='d-flex flex-column'>
+
+              <div className='d-flex flex-row mt-2'>
+                <span className="h1 fw-bold mb-0">Donatech</span>
+              </div>
+
+              <h5 className="fw-normal my-4 pb-3" style={{letterSpacing: '1px'}}>Sign into your account</h5>
+
+                <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+                <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+
+              <MDBBtn className="mb-4 px-5" color='dark' size='lg'>Login</MDBBtn>
+              <p className="mb-5 pb-lg-2" style={{color: '#393f81'}}>No tienes cuenta aun? <a href="./Register.jsx!" style={{color: '#393f81'}}>Register here</a></p>
+
+              <div className='d-flex flex-row justify-content-start'>
+                <a href="#!" className="small text-muted me-1">Terms of use.</a>
+                <a href="#!" className="small text-muted">Privacy policy</a>
+              </div>
+
+            </MDBCardBody>
+          </MDBCol>
+
+        </MDBRow>
+      </MDBCard>
+
+    </MDBContainer>
+  );
+}
+
+export default App;
